4dbc66a673bd1f011a8232c3e3a8e865839d0eba
khasec <khasec1@gmail.com>
2021-05-21 07:56:11 +0200

    Create out_2_fib2.py

---
+++ ou4_2/out_2_fib2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from fib import fib_py
+from time import perf_counter as pc
+import matplotlib.pyplot as plt
+from heltal import Heltal
+
+print(Heltal(47).fib())
+n1=30
+n2=40
+time_cpp=[]
+n_cpp=[]
+time_py=[]
+n_py=[]
+print('hej')
+# for n in range(n1,n2):
+
+#     k=Heltal(n)
+#     print(f'fib(n) for C++ with n={n}' )
+#     start_cpp = pc()
+#     print(k.fib())
+#     end_cpp = pc()
+#     print(f'Time in seconds {round(end_cpp-start_cpp, 2)}')
+#     time_cpp.append(round(end_cpp-start_cpp, 2))
+#     n_cpp.append(n)
+
+#     print(f'fib(n) for py with n={n}' )
+#     start_py = pc()
+#     print(fib_py(n))
+#     end_py = pc()
+#     print(f'Time in seconds {round(end_py-start_py, 2)}')
+#     time_py.append(round(end_py-start_py, 2))
+#     n_py.append(n)
+
+
+# print(f'fib(n) for C++ with n=47')
+# start_cpp = pc()
+# print(Heltal(47).fib())
+# end_cpp = pc()
+# print(f'Time in seconds {round(end_cpp-start_cpp, 2)}')
+
+# plt.plot(n_cpp,time_cpp ,'bo',n_py, time_py,'ro')
+# plt.savefig('fib_c.png')

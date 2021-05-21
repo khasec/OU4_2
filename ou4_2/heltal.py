aae1a561c472290f4def81be0ff0e09a9a2addd3
khasec <khasec1@gmail.com>
2021-05-21 08:23:58 +0200

    forgott to save

--- ou4_2/heltal.py
+++ ou4_2/heltal.py
@@ -6,14 +6,23 @@ class Heltal(object):
 	def __init__(self, val):
 		lib.Heltal_new.argtypes = [ctypes.c_int]
 		lib.Heltal_new.restype = ctypes.c_void_p
+
 		lib.Heltal_get.argtypes = [ctypes.c_void_p]
 		lib.Heltal_get.restype = ctypes.c_int
+
+		lib.Heltal_fib.argtypes = [ctypes.c_void_p]
+		lib.Heltal_fib.restype = ctypes.c_int
+
 		lib.Heltal_set.argtypes = [ctypes.c_void_p,ctypes.c_int]
 		lib.Heltal_delete.argtypes = [ctypes.c_void_p]
+
 		self.obj = lib.Heltal_new(val)
 
 	def get(self):
 		return lib.Heltal_get(self.obj)
+
+	def fib(self):
+		return lib.Heltal_fib(self.obj)
 
 	def set(self, val):
 		lib.Heltal_set(self.obj, val)

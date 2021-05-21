5453ae6e7824df8eeeba0b3974ab971495749af6
khasec <khasec1@gmail.com>
2021-05-21 07:51:54 +0200

    syntax fix

--- ou4_2/out_2_fib.py
+++ ou4_2/out_2_fib.py
@@ -5,7 +5,7 @@ from time import perf_counter as pc
 import matplotlib.pyplot as plt
 from heltal import Heltal
 
-
+print(Heltal(47).fib())
 n1=30
 n2=40
 time_cpp=[]
@@ -13,31 +13,31 @@ n_cpp=[]
 time_py=[]
 n_py=[]
  
-for n in range(n1,n2):
+# for n in range(n1,n2):
 
-    k=Heltal(n)
-    print(f'fib(n) for C++ with n={n}' )
-    start_cpp = pc()
-    print(k.fib())
-    end_cpp = pc()
-    print(f'Time in seconds {round(end_cpp-start_cpp, 2)}')
-    time_cpp.append(round(end_cpp-start_cpp, 2))
-    n_cpp.append(n)
+#     k=Heltal(n)
+#     print(f'fib(n) for C++ with n={n}' )
+#     start_cpp = pc()
+#     print(k.fib())
+#     end_cpp = pc()
+#     print(f'Time in seconds {round(end_cpp-start_cpp, 2)}')
+#     time_cpp.append(round(end_cpp-start_cpp, 2))
+#     n_cpp.append(n)
 
-    print(f'fib(n) for py with n={n}' )
-    start_py = pc()
-    print(fib_py(n))
-    end_py = pc()
-    print(f'Time in seconds {round(end_py-start_py, 2)}')
-    time_py.append(round(end_py-start_py, 2))
-    n_py.append(n)
+#     print(f'fib(n) for py with n={n}' )
+#     start_py = pc()
+#     print(fib_py(n))
+#     end_py = pc()
+#     print(f'Time in seconds {round(end_py-start_py, 2)}')
+#     time_py.append(round(end_py-start_py, 2))
+#     n_py.append(n)
 
 
-print(f'fib(n) for C++ with n=47')
-start_cpp = pc()
-print(Heltal(47).fib())
-end_cpp = pc()
-print(f'Time in seconds {round(end_cpp-start_cpp, 2)}')
+# print(f'fib(n) for C++ with n=47')
+# start_cpp = pc()
+# print(Heltal(47).fib())
+# end_cpp = pc()
+# print(f'Time in seconds {round(end_cpp-start_cpp, 2)}')
 
-plt.plot(n_cpp,time_cpp ,'bo',n_py, time_py,'ro')
-plt.savefig('fib_c.png')
+# plt.plot(n_cpp,time_cpp ,'bo',n_py, time_py,'ro')
+# plt.savefig('fib_c.png')
